698567c3093b485835f5bd3037c2c582c170b59e
Guilherme Pires <guilhermeemanuel45@gmail.com>
2024-09-23 20:18:12 -0300

    api_usuario

--- api_usuarios/index.js
+++ api_usuarios/index.js
@@ -14,12 +14,35 @@ const clientes = [{
     senha: "123456"
 }];
 
-
 app.post("/cadastro", (request, response) => {
 
-    clientes.push(request.body);
+    const {id, nome, email, senha } = request.body;
+
+    if(!id){
+        return response.status(400).send("É obrigatorio enviar o campo id!");
+    }else if(!nome){
+        return response.status(400).send("É obrigatorio enviar o campo nome!");
+    }else if(!email){
+        return response.status(400).send("É obrigatorio enviar o campo email!");
+    }else if(!senha){
+        return response.status(400).send("É obrigatorio enviar o campo senha!");
+    }
+
+    // for(let i=0; i < clientes.length; i++){
+    //     if(clientes[i].id == id){
+    //         return response.status(404).send(`O código ${id} já está em uso!`)
+    //     }
+    // }
+
+    const existe_id = clientes.filter(item => item.id == id);
     
-    response.status(200).send("cadastro");
+    if(existe_id){
+        return response.status(404).send(`O código ${id} já está em uso!`)
+    }else{
+        clientes.push(request.body);
+    }
+    
+    return response.status(200).send(request.body);
 
 })
 
