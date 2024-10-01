7baeda8448fdc43ce85cc09bb54ba4a7f51221d8
Guilherme Pires <guilhermeemanuel45@gmail.com>
2024-10-01 19:43:04 -0300

    api_mvc

--- api_mvc/routes/cliente.router.js
+++ api_mvc/routes/cliente.router.js
@@ -3,7 +3,10 @@ const clienteController = require('../controller/cliente.controller');
 
 const routes = express.Router();
 
-
-routes.post("/cadastro", clienteController);
+routes.post("/cadastro", clienteController.cadastro);
+routes.get("/consultar", clienteController.consulta);
+routes.get("/consultar/:id([0-9]+)", clienteController.consultaPorID);
+routes.delete("/deletar/:id([0-9]+)", clienteController.deletar);
+routes.put("/atualizar", clienteController.atualizar);
 
 module.exports = routes;
--- api_mvc/routes/nota.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get("/nota", (req, res) => {
-    res.send("Rota de Notas");
-})
-
-module.exports = router;
