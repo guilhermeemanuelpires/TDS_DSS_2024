ea2786919b06b2aea575f4a9061d53da8dcdd978
Guilherme Pires <guilhermeemanuel45@gmail.com>
2024-10-09 22:12:01 -0300

    api_mercado

---
+++ api_mercado/controller/cliente.controller.js
@@ -0,0 +1,9 @@
+
+module.exports = ({
+    cadastro: (req, res) => { },
+    consultar: (req, res) => {
+        res.send("Funcionou!");
+    },
+    atualizar: (req, res) => { },
+    deletar: (req, res) => { }
+})
