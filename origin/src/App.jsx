de697e722db194374a71970d2494b42e4b22960a
Guilherme Pires <guilhermeemanuel45@gmail.com>
2024-11-11 21:47:29 -0300

    project orion

---
+++ origin/src/App.jsx
@@ -0,0 +1,12 @@
+import Teste from "./Teste"
+
+function App() {
+    return (
+        <>
+            <h1>Ola Mundo! teste 123</h1>
+            <Teste />
+        </>
+    )
+}
+
+export default App
---
+++ origin/src/Teste.jsx
@@ -0,0 +1,10 @@
+
+import "./Teste.css";
+
+function Teste() {
+    return (
+        <h2>Componente 2</h2>
+    )
+}
+
+export default Teste
