cfa084f72415ac23865ced6362ca4ed02970b285
Guilherme Pires <guilhermeemanuel45@gmail.com>
2024-11-12 22:00:38 -0300

    Implementação useState input login

--- origin/src/App.jsx
+++ origin/src/App.jsx
@@ -1,35 +1,42 @@
 import Titulo from "./Titulo.jsx"
 import "./App.css"
-import { useState } from "react";
+import { useState } from "react";   
 
 function App() {
     const [contador, setContador] = useState(1);
-    const [nome, setNome] = useState("Guilherme");
-    const [email, setEmail] = useState("guilherme.emanuel@sistemafiep.org");
+    const [nome, setNome] = useState();
+    const [email, setEmail] = useState();
 
     return (
         <>
             <Titulo />
             <h2>{contador}</h2>
-            <button onClick={() => {
+            <button onClick={() => (
                 setContador(contador + 1)
-            }}>+</button>
-            <button onClick={() => {
+            )}>+</button>
+            <button onClick={() => (
                 setContador(contador - 1)
-            }}>-</button>
+            )}>-</button>
 
             <h1>Cadastro</h1>
             {nome} - {email}
             <div>
-                <input type="text" 
-                    placeholder="Nome" 
-                    onChange={(e)=>{
+                <input type="text"
+                    placeholder="Nome"
+                    onChange={(e) => (
                         setNome(e.target.value)
-                    }}/>
+                    )} />
 
-                <input type="email" placeholder="E-mail" />
-            </div>
-            
+                <input 
+                    type="email" 
+                    placeholder="E-mail"
+                    onChange={(e)=>(
+                        setEmail(e.target.value)
+                    )}
+                
+                />
+            </div>   
+               
         </>
     )
 }
