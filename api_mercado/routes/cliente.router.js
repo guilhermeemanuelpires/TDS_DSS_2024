65c167355a68d209efcdb2ca3437b47c0d066af0
Guilherme Pires <guilhermeemanuel45@gmail.com>
2024-11-25 22:16:15 -0300

    Contrução projeto cli_mercado

--- api_mercado/routes/cliente.router.js
+++ api_mercado/routes/cliente.router.js
@@ -2,7 +2,7 @@ const routes = require("express").Router();
 const clienteController = require("../controller/cliente.controller");
 
 // CRUD
-routes.post("/",[()=>{}], clienteController.cadastro);
+routes.post("/", clienteController.cadastro);
 routes.get("/", clienteController.consultar);
 routes.get("/:id([0-9]+)", clienteController.buscaPorId);
 routes.put("/", clienteController.atualizar);
--- origin/src/App.jsx
+++ origin/src/App.jsx
@@ -1,82 +1,19 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Titulo from "./Titulo.jsx"
-import "./App.css"
-
-const Api = axios.create({
-    baseURL: "http://localhost:8080/"
-});
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Alunos from "./pages/Alunos.jsx";
+import Professores from "./pages/Professores.jsx";
+import Turmas from "./pages/Turmas.jsx";
 
 function App() {
-    const [contador, setContador] = useState(1);
-    const [nome, setNome] = useState();
-    const [email, setEmail] = useState();
-
-    const [alunos, setAlunos] = useState();
-
-    useEffect(() => {
-        consultaAlunos();
-    }, [])
-
-    function salvarAluno() {
-        Api.post("aluno", {nome, email}).then((response)=>{
-            console.log(response.data)
-            consultaAlunos();
-        });   
-    }
-
-    function consultaAlunos(){
-        Api.get("aluno").then((response) => {
-            // console.log(response.data)
-            setAlunos(response.data)
-        });
-    }
-
     return (
         <>
-            <Titulo />
-            <h2>{contador}</h2>
-            <button onClick={() => (
-                setContador(contador + 1)
-            )}>+</button>
-            <button onClick={() => (
-                setContador(contador - 1)
-            )}>-</button>
-
-            <h1>Cadastro</h1>
-
-            <ol>
-                {alunos?.map((item, index) => (
-                    <li key={index}>{item.nome} - {item.email}</li>
-                ))}
-            </ol>
-
-            {nome} - {email}
-
-            <div>
-                <input type="text"
-                    placeholder="Nome"
-                    onChange={(e) => (
-                        setNome(e.target.value)
-                    )} />
-
-                <input
-                    type="email"
-                    placeholder="E-mail"
-                    onChange={(e) => (
-                        setEmail(e.target.value)
-                    )}
-                />
-                <button
-                    onClick={() => {
-                        salvarAluno()
-                    }}>
-                    Salvar
-                </button>
-            </div>
-
-
-
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Alunos/>}></Route>
+                    <Route  path="/alunos" element={<Alunos/>}></Route>
+                    <Route path="/professores" element={<Professores/>}></Route>
+                    <Route path="/turmas" element={<Turmas/>}></Route>
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
