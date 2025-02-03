f79f19f18332567b71d70bffed88ea7d69000f86
Guilherme Pires <guilhermeemanuel45@gmail.com>
2025-02-03 18:27:30 -0300

    removendo revisão

--- revisao/routes/index.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const pessoaRoute = require("./pessoa.route")
-
-const routes = express.Router();
-
-routes.use("/pessoas", pessoaRoute);
-
-module.exports = routes;
